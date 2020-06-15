60b05b4669746d305d02abebcb4df901cc6c88a5
Tasin Zaman <61430246+ZmnTasin@users.noreply.github.com>
2020-06-15 22:04:47 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ else {
             cursorwidth: "10px"
         });
     });
-}
 
-//SCROLL BEHAVIOUR SMOOTHE
-$(document).ready(function () {
+    //SCROLL BEHAVIOUR SMOOTHE
+    $(document).ready(function () {
         // Add smooth scrolling to all links
-    $(".scroll-btn, .navigation__link, .footer__link").on('click', function (event) {
+        $(".scroll-btn, .navigation__link, .footer__link").on('click', function (event) {
 
             // Make sure this.hash has a value before overriding default behavior
             if (this.hash !== "") {
@@ -37,7 +36,9 @@ $(document).ready(function () {
                 });
             } // End if
         });
-}   );
+    });
+
+}
 
 //CheckBox-Nav Collapse on Click
 $(".navigation__link").click(function () {
