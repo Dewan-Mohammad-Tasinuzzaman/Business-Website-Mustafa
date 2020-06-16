fc82d72b4d04fcb23fbdc8293c720867f08f301a
Tasin Zaman <61430246+ZmnTasin@users.noreply.github.com>
2020-06-17 01:54:03 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -46,107 +46,29 @@
 // VERSION - 2
 
 
+$(document).ready(function() {
+
+    //CheckBox-Nav Collapse on Click
+    $(".navigation__link").click(function () {
+        $(".navigation__checkbox").prop("checked", false);
+    })
 
 
-//CheckBox-Nav Collapse on Click
-$(".navigation__link").click(function () {
-    $(".navigation__checkbox").prop("checked", false);
-})
+    //SCROLL ANIMATION  
+    //*SEE HTML*
 
+    // Smooth-Scrolling
+    $(function () {
+        $("body").niceScroll({
+            cursorcolor: "#14ffec",
+            cursorwidth: "1rem",
+            cursorborder: "none",
+            cursorborderradius: "2px",
+            scrollspeed: 300,
+            mousescrollstep: 60,
+            background: "#212121"
+        });
+    });
 
-//SCROLL ANIMATION  
-const navbarToggler = document.querySelector(".scroll-btn, .navigation__link, .footer__link");
-const navbarMenu = document.querySelector(".scroll-btn, .navigation__link, .footer__link");
-const navbarLinks = document.querySelectorAll(".scroll-btn, .navigation__link, .footer__link");
+});
 
-navbarToggler.addEventListener("click", navbarTogglerClick);
-
-function navbarTogglerClick() {
-    navbarToggler.classList.toggle("open-navbar-toggler");
-    navbarMenu.classList.toggle("open");
-}
-
-// navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
-
-for (let i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].addEventListener("click", navbarLinkClick);
-}
-
-function navbarLinkClick(event) {
-
-    smoothScroll(event); // Call the "smoothScroll" function
-
-    if (navbarMenu.classList.contains("open")) { // Close navbarMenu in smaller screens
-        navbarToggler.click();
-    }
-
-}
-
-// Smooth-Scrolling
-
-
-function smoothScroll(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href") === "#" ? "header" : event.currentTarget.getAttribute("href");
-    const targetPosition = document.querySelector(targetId).offsetTop;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    const duration = 1000;
-    let start = null;
-
-    window.requestAnimationFrame(step);
-
-    function step(timestamp) {
-        if (!start) start = timestamp;
-        const progress = timestamp - start;
-        // window.scrollTo(0, distance*(progress/duration) + startPosition);
-        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-        if (progress < duration) window.requestAnimationFrame(step);
-    }
-}
-
-// Easing Functions
-
-function linear(t, b, c, d) {
-    return c * t / d + b;
-};
-
-function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-};
-
-function easeInOutCubic(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t * t + b;
-    t -= 2;
-    return c / 2 * (t * t * t + 2) + b;
-};
-
-
-
-//SMOOTHE MOUSEWHEEL SCROLLING
-
-function wheel(event) {
-    var delta = 0;
-    if (event.wheelDelta) { (delta = event.wheelDelta / 120); }
-    else if (event.detail) { (delta = -event.detail / 3); }
-
-    handle(delta);
-    if (event.preventDefault) { (event.preventDefault()); }
-    event.returnValue = false;
-}
-
-function handle(delta) {
-    var time = 1000;
-    var distance = 600;
-
-    $('html, body').stop().animate({
-        scrollTop: $(window).scrollTop() - (distance * delta)
-    }, time);
-}
-
-if (window.addEventListener) { window.addEventListener('DOMMouseScroll', wheel, false); }
-window.onmousewheel = document.onmousewheel = wheel;
